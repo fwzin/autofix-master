845078fba25f9faa5b1e91ffadd5e30dbcaece60
Francisco Costa <137823900+fwzin@users.noreply.github.com>
2024-09-21 20:18:36 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * MOBILE NAVBAR TOGGLE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+});
